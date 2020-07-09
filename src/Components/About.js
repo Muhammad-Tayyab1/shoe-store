401b3ad2e270f1f356d210212bdae4be4ccf9958
Muhammad-Tayyab1 <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-07-10 00:09:24 +0500

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ export const about = () => {
     return (
         <div>
             <h3 className="heading">Created by Muhammad Tayyab
-            <a href="https://github.com/Muhammad-Tayyab1" className="git">
+            <a href="https://github.com/Muhammad-Tayyab1/shoe-store" className="git">
                     GitHub Repo</a></h3><hr />
         </div>
     )
